dbd1454e83ed9dd1d08bfc32367c3f4a1e04f7f2
Sergej <ithinkdifferentno@gmail.com>
2021-05-03 17:39:52 +0300

    Application works, but need some corrections

--- script/script.js
+++ script/script.js
@@ -2,27 +2,24 @@ const $input = document.querySelector('.inp'),
 $checkbox = document.querySelector('.main-container__checkbox'),
 $btnLength = document.querySelector('.main-container__btnLength'),
 $btnFilter = document.querySelector('.main-container__btnFilter');
-let checkFetch = false;
-let $output = document.querySelector('.textOut');
-let data = [];
+let $output = document.querySelector('.textOut'),
+$stringsCount = document.querySelector('.stringsCount');
+let data, resultArr = [];
 
+getData('http://www.mrsoft.by/data.json');
 
-aboutFetch('http://www.mrsoft.by/data.json', $input.value);
-
-
-function aboutFetch(url) {
+function getData(url) {
     fetch(url)
     .then(response => response.json())
     .then(info =>  {
-        checkFetch = true;
         for(let item in info) {
             data = info[item];
-            // return data;
         }
     })
     .catch(function(err) {
         console.log(`Error: ${err}`);
-    });
+    }); 
+    return data;
 }
 
 document.addEventListener('click', (e) => {
@@ -30,28 +27,67 @@ document.addEventListener('click', (e) => {
         onBtnLengthClick();
     } else if(e.target === $btnFilter) {
         onBtnFilterClick();
+    } else if (e.target === $input) {
+        $output.innerHTML = '';
+        $stringsCount.innerHTML = '';
     }
 }); 
 
 function onBtnLengthClick() {
     inputCheck();
-    console.log(data);
+    // console.log(data);
 }
 
 function onBtnFilterClick() {
     inputCheck();
-    console.log(data);
+    // console.log(data);
+}
+
+function findForLength() {
+
 }
 
 function inputCheck() {
     const enteredValue = Number($input.value.trim());
     if(enteredValue === 0) {
         console.log('empty');
+        alert('Please, enter something!');
     } else if(isNaN(enteredValue)) {
-        // function for string entered
         console.log('string');
+        // function for string entered
+        data.forEach(item => {
+            if($checkbox.checked) {
+                // 'aAa' != 'aaa'
+                console.log(item.indexOf($input.value.trim()));
+                if(item.indexOf($input.value.trim()) != -1) {
+                    resultArr.push(item);
+                }
+            } else {
+                // 'aAa' === 'aaa'
+                console.log(item.indexOf($input.value.trim()));
+                if(item.toLowerCase().indexOf($input.value.toLowerCase().trim()) != -1) {
+                    resultArr.push(item);
+                }
+            }
+        });
+        $output.innerHTML = `Output strings: <br> ${resultArr}`;
+        // $stringsCount.innerHTML = resultArr.length;
+        $stringsCount.innerHTML = `Amount of output strings: <br> ${resultArr.length}`;
+        resultArr = [];
+
     } else if(!isNaN(enteredValue)) {
         // function for number entered
         console.log('number');
+    {
+            data.forEach(item => {
+                if(item.length > enteredValue) {
+                    console.log(item.length);
+                    resultArr.push(item);
+            }});
+            // $output.innerHTML = resultArr;
+            $output.innerHTML = `Output strings: <br> ${resultArr}`;
+            $stringsCount.innerHTML = `Amount of output strings: <br> ${resultArr.length}`;
+            resultArr = [];
+        }
     }
 }
